21c55701851a40ddf1516999f74ac57c8e9f2706
Loreny Ielpo <lorenyielpo@gmail.com>
2019-05-06 22:11:12 -0300

    exercicios

--- exe-cep/script.js
+++ exe-cep/script.js
@@ -1,17 +1,17 @@
-let cep = document.getElementById('cep') //variável pega a tag input do cep
+const cep = document.getElementById('cep') //variável pega a tag input do cep
 cep.addEventListener('change', ()=>{ //adiciona um escutador de evento na tag cep quando ela é alterada, executada a arrow function
-    fetch('https://viacep.com.br/ws/' + cep.value + '/json') //http faz 3 requisão, melhor usar https faz apenas uma. O cep.value puxa o número digitado no input e envia o link com o cep que deseja buscar
-        .then(function (response) {
+    fetch(`https://viacep.com.br/ws/${cep.value}/json`) //http faz 3 requisão, melhor usar https faz apenas uma. O cep.value puxa o número digitado no input e envia o link com o cep que deseja buscar
+        .then((response) => {
             return response.json()
         })
-        .then(function (data) {
+        .then((data) => {
             console.log(data)
-            let estado = document.getElementById('estado').value = data.uf
-            let cidade = document.getElementById('cidade').value = data.localidade
-            let bairro = document.getElementById('bairro').value = data.bairro
-            let endereco = document.getElementById('endereco').value = data.logradouro
+            const estado = document.getElementById('estado').value = data.uf
+            const cidade = document.getElementById('cidade').value = data.localidade
+            const bairro = document.getElementById('bairro').value = data.bairro
+            const endereco = document.getElementById('endereco').value = data.logradouro
         })
-        .catch(function (erro) {
+        .catch((erro) => {
             console.log(erro)
         })
 })
