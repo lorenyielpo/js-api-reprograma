9b87a6c4c7cb1c4622dc22f3dceafd89739e5f38
Loreny Ielpo <lorenyielpo@gmail.com>
2019-05-08 22:20:37 -0300

    exercicios

--- exe-profiles/script.js
+++ exe-profiles/script.js
@@ -1,51 +1,42 @@
-const app = document.getElementById('root')
-const h1 = document.createElement('h1')
-h1.innerHTML = 'Colaboradores'
-const container = document.createElement('div')
-container.setAttribute('class', 'authors')
+const app = document.getElementById('root');
+const authors = document.createElement('div');
+authors.setAttribute('class', 'authors');
+app.appendChild(authors);
 
-app.appendChild(h1)
-app.appendChild(container)
-
-const request = new XMLHttpRequest()
-
-request.open('GET', 'https://randomuser.me/api/?results=10', true)
-
+const request = new XMLHttpRequest();
+request.open('GET', 'https://randomuser.me/api/?results=10', true);
 request.onload = function() {
-    const data = JSON.parse(this.response)
     
-    console.log(data);
-    if(request.status >= 200 && request.status < 400){
-        console.log('SUCESSO!!!')
-        data.results.forEach(item => {
-            const card = document.createElement('div')
-            card.setAttribute('class', 'card')
+const data = JSON.parse(this.response);
 
-            const image = document.createElement('img')
-            image.setAttribute('src', item.picture.large)
+if (request.status>=200 && request.status<400){
+    console.log('Sucesso')
+    data.results.forEach(pessoas => {
+        const card=document.createElement('div');
+        card.setAttribute('class', 'card');
+        authors.appendChild(card);
 
-            const name = document.createElement('h2')
-            name.innerHTML = `${item.name.first} ${item.name.last}`
+        const img = document.createElement('img');
+        img.setAttribute("src", pessoas.picture.large);
+        card.appendChild(img);
 
-            const local = document.createElement('h3')
-            local.innerHTML = `${item.location.city}, ${item.nat}`
+        const name = document.createElement('h2');
+        name.textContent = pessoas.name.first + " " + pessoas.name.last;
+        card.appendChild(name);
 
-            const email = document.createElement('p')
-            email.innerHTML = item.email
+        const local = document.createElement('h3');
+        local.textContent = pessoas.location.city + ", " + pessoas.nat;
+        card.appendChild(local);
 
-            const phone = document.createElement('p')
-            phone.innerHTML = item.phone
-            
-            container.appendChild(card)
-            card.appendChild(image)
-            card.appendChild(name)
-            card.appendChild(local)
-            card.appendChild(email)
-            card.appendChild(phone)
-          });
-    }else{
-        console.log('erro :C')
-    };
+        const email = document.createElement('p');
+        email.textContent = pessoas.email;
+        card.appendChild(email);
+
+        const phone = document.createElement ('p');
+        phone.textContent = pessoas.cell;
+        card.appendChild(phone);
+
+    });
 }
-
-request.send();
+}
+request.send()
--- exe-profiles/script2.js
+++ exe-profiles/script2.js
@@ -1,43 +1,42 @@
-const app = document.getElementById('root')
-const h1 = document.createElement('h1')
-h1.innerHTML = 'Colaboradores'
-const container = document.createElement('div')
-container.setAttribute('class', 'authors')
-
-app.appendChild(h1)
-app.appendChild(container)
+const app = document.getElementById('root');
+const authors = document.createElement('div');
+authors.setAttribute('class', 'authors');
+app.appendChild(authors);
 
 fetch('https://randomuser.me/api/?results=10')
 .then(function(response){
-    return response.json()
+    return response.json();
 })
 .then(function(data){
-    data.results.forEach(item => {
-        const card = document.createElement('div')
-        card.setAttribute('class', 'card')
+    console.log("SUCESSO!!")
+    console.log(data)
+    data.results.forEach(pessoas => {
+        const card=document.createElement('div');
+        card.setAttribute('class', 'card');
+        authors.appendChild(card);
 
-        const image = document.createElement('img')
-        image.setAttribute('src', item.picture.large)
+        const img = document.createElement('img');
+        img.setAttribute("src", pessoas.picture.large);
+        card.appendChild(img);
 
-        const name = document.createElement('h2')
-        name.innerHTML = `${item.name.first} ${item.name.last}`
+        const name = document.createElement('h2');
+        name.textContent = pessoas.name.first + " " + pessoas.name.last;
+        card.appendChild(name);
 
-        const local = document.createElement('h3')
-        local.innerHTML = `${item.location.city}, ${item.nat}`
+        const local = document.createElement('h3');
+        local.textContent = pessoas.location.city + ", " + pessoas.nat;
+        card.appendChild(local);
 
-        const email = document.createElement('p')
-        email.innerHTML = item.email
+        const email = document.createElement('p');
+        email.textContent = pessoas.email;
+        card.appendChild(email);
 
-        const phone = document.createElement('p')
-        phone.innerHTML = item.phone
-        
-        container.appendChild(card)
-        card.appendChild(image)
-        card.appendChild(name)
-        card.appendChild(local)
-        card.appendChild(email)
-        card.appendChild(phone)
+        const phone = document.createElement ('p');
+        phone.textContent = pessoas.cell;
+        card.appendChild(phone);
+
     });
+    
 })
 .catch(function(erro){
     console.log(erro)
